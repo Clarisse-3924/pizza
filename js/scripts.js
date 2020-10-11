903f7681ff7923a037e0ca4c9350f7e6be20fc95
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-10-11 18:57:04 +0200

    Modifying styles,js and html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,60 @@
 $(document).ready(function() {
-      $("#text-center").submit(function(event) {
-        function size() {
-          var sizes= $("input:radio[name=size]:checked").val();
-          return parseInt(sizes);
-        }
-        function crust() {
-          var crusts= $("input:radio[name=crust]:checked").val();
-          return parseInt(crusts);
-        }
-        function topping() {
-          var toppings = document.getElementById("topping").value;
-          return parseInt(toppings);
-        }
-        function number() {
-          var pizzaNumber = document.getElementById("quantity").value;
-          return parseInt(pizzaNumber);
-        }
-        function delivery() {
-            var delivery = document.getElementById("delivery").value;
-            return parseInt(delivery);
-          }
-        function myOrder( size, crust, topping, quantity,delivery) {
-          this.newSize = size;
-          this.newCrust = crust;
-          this.newTopping = topping;
-          this.newQuantity = quantity;
-          this.newDelivery=delivery;
-        }
-        var userInput = new myOrder(size(), crust(), topping(), number(),delivery());
-        var totalCost =(userInput.newSize +userInput.newCrust + userInput.newTopping+userInput.newDelivery) * userInput.newQuantity;
-        alert("Your charges for Pizza" + totalCost);
-    prompt("enter your email address");
-    prompt("enter your phone number");
-    prompt("enter your location");
-    alert("Your pizza will be delivered");
-       
+  $("#form").submit(function(event){
+      
+    function size() {
+      var sizes= $("input:radio[name=sizes]:checked").val();
+      return parseInt(sizes);
+    }
+    function crust() {
+      var crusts= $("input:radio[name=crusts]:checked").val();
+      return parseInt(crusts);
+    }
+    function topping() {
+      var toppings= $("input:radio[name=toppings]:checked").val();
+      return parseInt(toppings);
+    }
+    function delivery() {
+      var delivery1 = document.getElementById("delivery").value;
+      return parseInt(delivery1);
+    }
+    function quantity() {
+      var number = document.getElementById("inputNumber").value;
+      return parseInt(number);
+    }
+    function myOrder(size, crust, topping, quantity,delivery) {
+      this.mySize = size;
+      this.myCrust = crust;
+      this.myToppings = topping;
+      this.myNumber = quantity;
+      this.myDelivery = delivery;
+
+    }
+    var inputs = new myOrder(size(), crust(), topping(), quantity(),delivery());
+    var totalCost =
+      (inputs.mySize +
+        inputs.myCrust+
+        inputs.myDelivery+
+        inputs.myToppings) *
+        inputs.myNumber;
+
+        prompt("enter your email address");
+        prompt("enter your phone number");
+        prompt("enter your location");
+        alert("Your pizza will be delivered");
+        
+        $(".check").show();
+        document.getElementById("Size").innerHTML=inputs.mySize + (" Rwf");
+        document.getElementById("Crust").innerHTML= inputs.myCrust + (" Rwf");
+        document.getElementById("Toppings").innerHTML=inputs.myToppings + (" Rwf");
+        document.getElementById("Delivery").innerHTML=inputs.myDelivery + (" Rwf");
+        document.getElementById("answer").innerHTML=totalCost+(" Rwf");
+    event.preventDefault();
+  });
+});
     
-      });
-    });
+
+
+    
     $(document).ready(function(){
       $("#form1").submit(function(){
         var name=$("input#name").val();
